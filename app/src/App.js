d92706d2733d3901749d4a3fd171a0cbb045981e
Kartikey <codekartikey@gmail.com>
2023-02-19 17:39:46 +0530

    add app

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='Page'>
+    <div className='ButtonContainer'>
+    <button className='Button'>View Teachers</button>
+    <button className='Button'>View Cameras</button>
+    </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
